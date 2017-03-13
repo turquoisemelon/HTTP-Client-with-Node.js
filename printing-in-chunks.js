bf45f607ff0bfa633329cf0fee2760acb23a20a4
meltem kilic <melltemkilic@gmail.com>
2017-03-13 16:31:04 +0000

    printing data with buffered strings

--- printing-in-chunks.js
+++ printing-in-chunks.js
@@ -2,24 +2,24 @@ var https = require('https');
 
 function getAndPrintHTMLChunks () {
 
-
-
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
 
   https.get(requestOptions, function(response){
+    var test = "";
 
     // set encoding of received data to UTF-8
     response.setEncoding('utf8');
 
     // the callback is invoked when a 'data' chunck is received
     response.on('data', function (data) {
-      console.log(data + '\n');
+      test += data;
     });
+
     response.on('end', function() {
-      console.log('Response stream complete.');
+      console.log(test);
     });
   });
 }
